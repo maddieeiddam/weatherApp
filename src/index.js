665197ac5ebd8103fef6c29decaddd37a7ec6722
Maddie Jennings <madisonlynnjennings@gmail.com>
2021-05-03 21:24:00 -0400

    Set up webpack, babel, express, react, and basic file structure

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Index = () => {
+  return (
+    <div>SooOOoooo much weather</div>
+  )
+};
+
+ReactDOM.render(<Index />, document.getElementById('app'));
